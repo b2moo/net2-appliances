14aed64ab3be4ba1fb84925bac83f69dc02a0d2f
Daniel STAN <ds@tudo.re>
2023-05-03 15:45:43 +0200

    forgot registry again in login

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker login -u students23 -p PYXYgjQAkesJa12YtKs7
+docker login registry.cri.epita.fr -u students23 -p PYXYgjQAkesJa12YtKs7
 
 # download stuff by hand, because GNS3 seems to fail this :(
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-internet
