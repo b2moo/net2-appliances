d7a3e3a8d4348e96c227deb54986b61ec5e910ea
Daniel STAN <ds@tudo.re>
2023-05-11 22:20:55 +0200

    add an illustration

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to install
+
+1. Clone (or unzip) this repo (folder)
+2. Run `./install.sh`
+3. Start GNS3
+4. Import every appliance (`.gns3a` file)
+	
+	!["New template" then "import an appliance file"](./howto.png)
--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+CONFIG=~/GNS3
+
 docker login registry.cri.epita.fr -u students23 -p PYXYgjQAkesJa12YtKs7
 
 # download stuff by hand, because GNS3 seems to fail this :(
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-internet
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-router
+
+
+# custom symbols
+cp internet.png $CONFIG/symbols/
+cp letsencrypt-logo.png $CONFIG/symbols/
+
