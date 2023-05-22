5f01e16e5bcc18042dc9b9fd5314fefe36f89eee
Daniel STAN <ds@tudo.re>
2023-05-22 08:28:38 +0200

    add a net2-machine (just a rebranded net2-router)

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ docker login registry.cri.epita.fr -u students23 -p PYXYgjQAkesJa12YtKs7
 
 # download stuff by hand, because GNS3 seems to fail this :(
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-internet
+docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-machine
+#docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-pebble
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-router
 
 
 # custom symbols
 cp internet.png $CONFIG/symbols/
 cp letsencrypt-logo.png $CONFIG/symbols/
-
+cp linux_guest.svg $CONFIG/symbols/
