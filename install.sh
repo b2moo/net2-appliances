8c401b7ef889881a2ad54ae56f39ae187647c154
Daniel STAN <ds@tudo.re>
2023-05-22 08:57:14 +0200

    add net2-secretuser

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ docker login registry.cri.epita.fr -u students23 -p PYXYgjQAkesJa12YtKs7
 # download stuff by hand, because GNS3 seems to fail this :(
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-internet
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-machine
+docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-secretuser
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-pebble
 docker pull registry.cri.epita.fr/daniel.stan/net2-appliances/net2-router
 
