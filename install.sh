6518745f8bdb879bda199f9fddc4a0361d883bff
Daniel STAN <ds@tudo.re>
2023-07-17 18:24:43 +0200

    import appliances template automatically, the first time

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-CONFIG=~/GNS3
+DATA_DIR=~/GNS3
 REGISTRY=registry.cri.epita.fr/daniel.stan/net2-appliances
+CONF=~/.config/GNS3/2.2/gns3_server.conf
+HOST=localhost:3080
+
+if grep -iq "^auth *= *True" $CONF; then
+    login=$(sed "s/^user *= *\(.*\)$/\1/; t; d" $CONF)
+    pass=$(sed "s/^password *= *\(.*\)$/\1/; t; d" $CONF)
+    HOST=$login:$pass@$HOST
+fi
 
 docker login registry.cri.epita.fr -u students23 -p PYXYgjQAkesJa12YtKs7
 
@@ -16,17 +24,17 @@ docker pull $REGISTRY/net2-webserver
 
 # custom symbols
 echo "### INSTALL custom symbols"
-cp internet.png $CONFIG/symbols/
-cp letsencrypt-logo.png $CONFIG/symbols/
-cp linux_guest.svg $CONFIG/symbols/
-cp secret_user.svg $CONFIG/symbols/
-cp webserver.svg $CONFIG/symbols/
+cp internet.png $DATA_DIR/symbols/
+cp letsencrypt-logo.png $DATA_DIR/symbols/
+cp linux_guest.svg $DATA_DIR/symbols/
+cp secret_user.svg $DATA_DIR/symbols/
+cp webserver.svg $DATA_DIR/symbols/
 
 
 echo "### IMPORTING APPLIANCES"
-wget http://localhost:3080/v2/templates --post-file=./net2-internet.tpl -O -
-wget http://localhost:3080/v2/templates --post-file=./net2-router.tpl -O -
-wget http://localhost:3080/v2/templates --post-file=./net2-machine.tpl -O -
-wget http://localhost:3080/v2/templates --post-file=./net2-secretuser.tpl -O -
-wget http://localhost:3080/v2/templates --post-file=./net2-pebble.tpl -O -
-wget http://localhost:3080/v2/templates --post-file=./net2-webserver.tpl -O -
+wget http://$HOST/v2/templates --post-file=./net2-internet.tpl -O -
+wget http://$HOST/v2/templates --post-file=./net2-router.tpl -O -
+wget http://$HOST/v2/templates --post-file=./net2-machine.tpl -O -
+wget http://$HOST/v2/templates --post-file=./net2-secretuser.tpl -O -
+wget http://$HOST/v2/templates --post-file=./net2-pebble.tpl -O -
+wget http://$HOST/v2/templates --post-file=./net2-webserver.tpl -O -
