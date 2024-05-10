ff629d4235c612003e04205032c05e300276c41f
Daniel STAN <ds@tudo.re>
2024-05-10 14:25:46 +0200

    refactor

---
+++ config.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+
+if ! which jq > /dev/null; then
+    echo "Please install \"jq\" first !"
+    echo "Ubuntu/Debian: sudo apt install jq"
+    echo "nixOS: nix-shell -p jq"
+    exit 1
+fi
+
+# useless ?
+REGISTRY=registry.cri.epita.fr/daniel.stan/net2-appliances
+# Gitlab/Registry credidentials
+LOGIN=net2-2024
+PASS=ZMddwrbApgpMMozoBh1L
+
+# Template files for GNS3
+TEMPLATES="net2-internet.tpl net2-router.tpl net2-machine.tpl net2-secretuser.tpl net2-pebble.tpl"
+
+# Docker images of these templates
+IMAGES=$(for t in $TEMPLATES; do jq -r '.image + ""' $t; done | sort | uniq)
+
+# Symbols (icons in GNS3) of these templates
+SYMBOLS=$(for t in $TEMPLATES; do jq -r '.symbol + ""' $t; done | grep -v "^:" | sort | uniq)
+
+DATA_DIR=~/GNS3
+CONF=~/.config/GNS3/2.2/gns3_server.conf
+HOST=localhost:3080
+
+# Look for a password protected GNS3
+if grep -iq "^auth *= *True" $CONF; then
+    login=$(sed "s/^user *= *\(.*\)$/\1/; t; d" $CONF)
+    pass=$(sed "s/^password *= *\(.*\)$/\1/; t; d" $CONF)
+    HOST=$login:$pass@$HOST
+fi
+
--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-DATA_DIR=~/GNS3
-REGISTRY=registry.cri.epita.fr/daniel.stan/net2-appliances
-CONF=~/.config/GNS3/2.2/gns3_server.conf
-HOST=localhost:3080
-# Gitlab credidentials
-LOGIN=net2-2024
-PASS=ZMddwrbApgpMMozoBh1L
+. ./config.sh
 
 # Is it the latest version of this repository ?
 oldhash=$(git rev-parse HEAD)
@@ -19,46 +13,22 @@ if [ "$oldhash" != "$newhash" ]; then
     exec ./install.sh
 fi
 
-
-
-# Look for a password protected GNS3
-if grep -iq "^auth *= *True" $CONF; then
-    login=$(sed "s/^user *= *\(.*\)$/\1/; t; d" $CONF)
-    pass=$(sed "s/^password *= *\(.*\)$/\1/; t; d" $CONF)
-    HOST=$login:$pass@$HOST
-fi
-
 # Checking that we can connect to GNS3 server
-if wget http://$HOST/v2/computes -O /dev/null; then
-    echo "Connected to server (./)"
+if wget http://$HOST/v2/computes -O /dev/null 2> /dev/null; then
+    echo "Connected to GNS3 server (./)"
 else
-    echo "!!!! Cannot connect to GNS3, launch it first !!!!"
+    echo "/!\\ Cannot connect to GNS3, launch it first /!\\"
     exit 1
 fi
 
-
-docker login registry.cri.epita.fr -u $LOGIN -p $PASS
-
-# download stuff by hand, because GNS3 seems to fail this :(
-echo "### PULL DOCKER IMAGES"
-docker pull $REGISTRY/net2-internet
-docker pull $REGISTRY/net2-secretuser
-docker pull $REGISTRY/net2-pebble
-docker pull $REGISTRY/net2-router
-docker pull $REGISTRY/net2-webserver
-
-
 # custom symbols
 echo "### INSTALL custom symbols"
-cp internet.png $DATA_DIR/symbols/
-cp letsencrypt-logo.png $DATA_DIR/symbols/
-cp linux_guest.svg $DATA_DIR/symbols/
-cp secret_user.svg $DATA_DIR/symbols/
-cp webserver.svg $DATA_DIR/symbols/
-
+for symb in $SYMBOLS; do
+    cp $symb $DATA_DIR/symbols/
+done
 
 echo "### IMPORTING APPLIANCES"
-for tpl in net2-internet.tpl net2-router.tpl net2-machine.tpl net2-secretuser.tpl net2-pebble.tpl; do
+for tpl in $TEMPLATES; do
     echo -n "  deleting first $tpl ..."
     id=$(jq -r '.["template_id"]' $tpl)
     if wget --method=DELETE http://$HOST/v2/templates/$id -O /dev/null 2> /dev/null; then
@@ -73,4 +43,13 @@ for tpl in net2-internet.tpl net2-router.tpl net2-machine.tpl net2-secretuser.tp
     else
         echo "failed"
     fi
+done
+
+# Login so that all image pull work (by GNS3, or "by hand")
+docker login $REGISTRY -u $LOGIN -p $PASS
+
+# Download stuff by hand
+echo "### PULL DOCKER IMAGES"
+for img in $IMAGES; do
+    docker pull $img
 done
